f8685463e8ce53ecb9111475cd5cdc4007c37d32
Kenneth Robbe <kenneth.robbe@frucon.net>
2021-12-10 08:41:54 +0100

    Day 1 challenges

---
+++ solvers/Day1.js
@@ -0,0 +1,52 @@
+import BaseSolver from './BaseSolver.js';
+
+/**
+ * Solver for https://adventofcode.com/2021/day/1
+ */
+export default class Day1 extends BaseSolver {
+    day = 1;
+    challenges = [
+        {
+            method: 'countIncreases',
+            inputData: 'input',
+            testData: 'test',
+            expectedTestResult: 7
+        },
+        {
+            method: 'countSlidingWindowIncreases',
+            inputData: 'input',
+            testData: 'test',
+            expectedTestResult: 5
+        }
+    ];
+
+    parseDataPoint(d) {
+        return parseInt(d, 10);
+    }
+
+    // count the number of times a depth measurement increases from the previous measurement
+    countIncreases(data) {
+        return data.filter((d, i, arr) => i > 0 && arr[i - 1] < d).length;
+    }
+
+    // Considering sums of a three-measurement sliding window,
+    // count the number of times the sum of measurements in this sliding window
+    // increases from the previous sum
+    countSlidingWindowIncreases(data) {
+        const slidingWindowWidth = 3;
+        const slidingWindowSums = data
+            .map((d, i, arr) => {
+                if (i + slidingWindowWidth > arr.length) {
+                    return;
+                }
+
+                const slidingWindow = arr.slice(i, i + slidingWindowWidth);
+                const sum = slidingWindow.reduce((total, n) => total + n, 0);
+
+                return sum;
+            })
+            .filter((w) => typeof w !== 'undefined');
+
+        return this.countIncreases(slidingWindowSums);
+    }
+}
